143086d2ea7e16d30ee1959244139b0d80ba1b51
Cao Thanh Long <ltcaosj@gmail.com>
2019-11-14 15:23:13 +0700

    bump version

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import { NativeModules } from 'react-native';
 const { LKAppAnalytics } = NativeModules;
 
 export const setCurrentScreen = screenName => {
-    
+    LKAppAnalytics.setCurrentScreen(screenName)
+};
+
+export const logEvent = (eventName, parameters) => {
+    LKAppAnalytics.logEvent(eventName, parameters)
 };
 
 export { LKAppAnalytics };
